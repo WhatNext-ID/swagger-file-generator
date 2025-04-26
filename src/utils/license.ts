ee804a5d93ceb3d5b79efb928912bbbc6def39bd
Z4nR <ari.comunity1@gmail.com>
2025-04-26 10:18:56 +0700

    chore: add licenses list for choose list option

---
+++ src/utils/license.ts
@@ -0,0 +1,34 @@
+export const licenses = [
+  {
+    name: 'GNU AGPLv3',
+    url: 'https://www.gnu.org/licenses/agpl-3.0.txt',
+  },
+  {
+    name: 'GNU GPLv3',
+    url: 'https://www.gnu.org/licenses/gpl-3.0-standalone.html',
+  },
+  {
+    name: 'GNU LGPLv3',
+    url: 'https://www.gnu.org/licenses/lgpl-3.0-standalone.html',
+  },
+  {
+    name: 'Mozilla Public License 2.0',
+    url: 'https://www.mozilla.org/en-US/MPL/2.0/',
+  },
+  {
+    name: 'Apache License 2.0',
+    url: 'https://www.apache.org/licenses/LICENSE-2.0',
+  },
+  {
+    name: 'MIT License',
+    url: 'https://opensource.org/license/mit',
+  },
+  {
+    name: 'Boost Software License 1.0',
+    url: 'https://www.boost.org/LICENSE_1_0.txt',
+  },
+  {
+    name: 'The Unlicense',
+    url: 'https://unlicense.org/',
+  },
+];
